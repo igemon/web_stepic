2f77591afe9f3eb848ebede88f3a33043bc3f88f
Fedorov Artem <a.fedorov@pflb.ru>
2017-04-03 11:39:42 +0300

    ololo

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def app(environ, start_response):
-	status="200 OK"
-	responce_headers=[('Content-Type', 'text/plain')]
-    start_response(status,responce_headers)
-	get_params = environ['QUERY_STRING'].split("&")
-	resp_data = [item + "\r\n" for item in get_params]
+    status = '200 OK'
+    response_headers = [('Content-type','text/plain')]
+    start_response(status, response_headers)
+	get_params = environ['QUERY_STRING'].split('&')
+	resp_data = [item + '\r\n' for item in get_params]
     return resp_data
