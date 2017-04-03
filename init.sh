d24a137fd84ac9ca039d2126d5c6b82ad5a76f71
Fedorov Artem <a.fedorov@pflb.ru>
2017-04-03 10:55:36 +0300

    qwer

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
