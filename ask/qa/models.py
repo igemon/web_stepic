2cc27f98423f169515796972e3dfae49c08f67cf
igemon <zetsigemon@gmail.com>
2017-04-05 12:39:59 +0000

    it works again

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -9,7 +9,7 @@ class QuestionManager(models.Manager):
                 pass 
 
 class Question(models.Model):
-	objects = QuestionManager()
+    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
