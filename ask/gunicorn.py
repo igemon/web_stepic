4ad2a18ddfda9b2079188203e244a1bfcfde2554
Fedorov Artem <a.fedorov@pflb.ru>
2017-04-05 15:11:51 +0300

    models

--- ask/gunicorn.py
+++
@@ -1,5 +0,0 @@
-bind = "0.0.0.0:8000"
-workers = 10
-pythonpath = "/home/box/web/ask"
-errorlog = "/home/box/web/etc/ask-error.log"
-loglevel = "debug"
---
+++ ask/qa/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+class QuestionManager(models.Manager):                                          
+        def new():                                                              
+                pass                                                            
+        def popular():                                                          
+                pass 
+
+class Question(models.Model):
+	objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, related_name="question_author")
+    likes = models.ManyToManyField(
+        User, related_name="question_like", blank=True)
+
+    class Meta:
+        ordering = ('-added_at',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('question_detail', kwargs={'pk': self.pk})
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+
+    class Meta:
+        ordering = ('added_at',)
+
+    def __str__(self):
+        return 'Answer by {}'.format(self.author)
