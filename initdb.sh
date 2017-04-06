57d0e28f6887e12825dea721bc681e0e18af8189
igemon <zetsigemon@gmail.com>
2017-04-06 10:10:08 +0000

    init files

---
+++ initdb.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE USER 'admin'@'localhost'"
+mysql -uroot -e "SET PASSWORD FOR 'admin'@'localhost' = PASSWORD('pass111')"
+mysql -uroot -e "CREATE DATABASE mybase"
+mysql -uroot -e "GRANT ALL ON mybse.* TO 'admin'@'localhost'"
+python ~/web/ask/manage.py makemigrations
+python ~/web/ask/manage.py migrate
